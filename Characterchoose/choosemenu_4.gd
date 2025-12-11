e75ec1c014a348ee6fa9f9fff06354d50759d5ae
Uezbaev <tynalbekov_m@iuca.kg>
2025-12-11 08:37:07 +0600

    instruction menu
    i add some new stilistic disain elements

--- Characterchoose/choosemenu_4.gd
+++ Characterchoose/choosemenu_4.gd
@@ -3,3 +3,7 @@ extends Control
 
 func _on_button_pressed() -> void:
 	var error = get_tree().change_scene_to_file("res://Characterchoose/choosemenu_3.tscn")
+
+
+func _on_button_6_pressed() -> void:
+	var error = get_tree().change_scene_to_file("res://main_menu/mainmenu.tscn")
